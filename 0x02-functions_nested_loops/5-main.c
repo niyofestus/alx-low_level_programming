a465d705bf1918a8643f4d6c89289caef2f50dd7
NIYITANGA Festus <43537970+niyofestus@users.noreply.github.com>
2022-06-08 12:37:02 +0200

    Update 5-main.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,12 +1,33 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    more_numbers();
+    int r;
+
+    r = print_sign(98);
+    putchar(',');
+    putchar(' ');
+    putchar(r + '0');
+    putchar('\n');
+    r = print_sign(0);
+    putchar(',');
+    putchar(' ');
+    putchar(r + '0');
+    putchar('\n');
+    r = print_sign(0xff);
+    putchar(',');
+    putchar(' ');
+    putchar(r + '0');
+    putchar('\n');
+    r = print_sign(-1);
+    putchar(',');
+    putchar(' ');
+    putchar(r + '0');
+    putchar('\n');
     return (0);
 }
