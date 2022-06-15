c8ad83877c3e59d7ab8ae1961713e913450d0f34
NIYITANGA Festus <43537970+niyofestus@users.noreply.github.com>
2022-06-15 19:22:08 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: integer
- * Return: void
+ * print_number - print an integer
+ * @n: integer to print
  */
-
 void print_number(int n)
 {
-int divisor = 1, i, resp;
-
-if (n < 0)
-{
-	_putchar('-');
-	n *= -1;
-}
-
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-
-}
-
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
+	}
+	else
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
 }
