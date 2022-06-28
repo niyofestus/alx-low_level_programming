408799e2171396ad194163d37ad07c0b873052fb
NIYITANGA Festus <43537970+niyofestus@users.noreply.github.com>
2022-06-28 06:28:59 +0200

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,66 +1,40 @@
 #include "main.h"
 
 /**
- * _isspace - check if a character is whitespace
- * @c: the character to check
+ * argstostr - concatenate all arguments into a new string
+ * @ac: the argument count
+ * @av: the argument vector
  *
- * Return: 1 is c is a whitespace character, otherwise 0
+ * Return: NULL memory allocation fails, ac is 0 or av is NULL,
+ * otherwise return a pointer to the new string
  */
-int _isspace(int c)
+char *argstostr(int ac, char **av)
 {
-	if (c == 0x20 || (c >= 0x09 && c <= 0x0d))
-		return (1);
-	return (0);
-}
+	char *cat, *chr;
+	int arg, size;
 
-
-/**
- * strtow - split a string into words
- * @str: a pointer to the string to split
- *
- * Return: NULL if memory allocation fails or if str is NULL or empty (""),
- * otherwise return a pointer to the array of words terminated by a NULL
- */
-char **strtow(char *str)
-{
-	char **words, *pos = str;
-	int w = 0, c;
-
-	if (!(str && *str))
+	if (!ac || !av)
 		return (NULL);
-	do {
-		while (_isspace(*pos))
-			++pos;
-		if (!*pos)
-			break;
-		while (*(++pos) && !_isspace(*pos))
+
+	for (arg = 0, size = 1; arg < ac; ++arg, ++size)
+	{
+		for (chr = av[arg]; *chr; ++chr, ++size)
 			;
-	} while (++w, *pos);
-	if (!w)
+	}
+
+	cat = (char *) malloc(sizeof(char) * size);
+
+	if (!cat)
 		return (NULL);
-	words = (char **) malloc(sizeof(char *) * (w + 1));
-	if (!words)
-		return (NULL);
-	w = 0, pos = str;
-	do {
-		while (_isspace(*pos))
-			++pos;
-		if (!*pos)
-			break;
-		for (str = pos++; *pos && !_isspace(*pos); ++pos)
-			;
-		words[w] = (char *) malloc(sizeof(char) * (pos - str + 1));
-		if (!words[w])
-		{
-			while (w >  0)
-				free(words[--w]);
-			free(words);
-			return (NULL);
-		}
-		for (c = 0; str < pos; ++c, ++str)
-			words[w][c] = *str;
-		words[w][c] = '\0';
-	} while (++w, *pos);
-	words[w] = NULL;
-	return (words);
+
+	for (arg = 0, size = 0; arg < ac; ++arg, ++size)
+	{
+		for (chr = av[arg]; *chr; ++chr, ++size)
+			cat[size] = *chr;
+		cat[size] = '\n';
+	}
+
+	cat[size] = '\0';
+
+	return (cat);
 }
